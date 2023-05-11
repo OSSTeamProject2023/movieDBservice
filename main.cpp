f8f78e5029b4c4e689168761d2163911266f8e75
최동빈 <dbchoi@choedoncBookPro.T-mobile.com>
2023-05-11 22:04:31 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+class DB_list {
+    string name;
+    string movie_name;
+    string seat;
+    int price;
+    string date;
+    int room;
+public:
+    void addInfo();
+    void deleteInfo();
+    void loadInfo();
+    void updateInfo();
+    void search_by_name();
+    void search_by_moviename();
+    void leftSeats();
+};
+
+
+void DB_list::addInfo(){
+
+}
+
+void DB_list::deleteInfo(){
+
+}
+ 
+void DB_list::loadInfo(){
+
+}
+
+void DB_list::updateInfo(){
+
+}
+
+void DB_list::search_by_name(){
+
+}
+
+void DB_list::search_by_moviename(){
+
+}
+
+void DB_list::leftSeats(){
+
+}
+
+void saveData(){
+
+}
+
+void loadData(){
+
+}
+
+int show_menu(){
+
+}
+
+int main(){
+    
+}
